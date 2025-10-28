b9ce0d3fc705dcc4ad2bb97f256402f8876249a7
Camilo <juan.londono2@estudiantesunibague.edu.co>
2025-10-27 19:28:00 -0500

    Router creado

--- routes/comics.js
+++ routes/comics.js
@@ -2,6 +2,6 @@ const comicsController = require("../controllers/comics")
 
 const comicsRouter = require("express").Router()
 
-comicsRouter.get("/todo", comicsController)
+comicsRouter.get("/todo", comicsController.getComics)
 
 module.exports = comicsRouter
